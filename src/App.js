c0937d3aff1a8c56b8a473344d401ca28c348c35
raifashafi <raifafisat@gmail.com>
2024-07-28 21:45:28 +0530

    project created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import CreatePost from './components/CreatePost';
 
 function App() {
   return (
-    <div>
-     <SignUp/>
+   <BrowserRouter>
+    <Routes>
+ <Route   path='/' element={<SignIn/>}/>
+ <Route   path='/signup' element={<SignUp/>}/>
+ <Route   path='/create' element={<CreatePost/>}/>
 
 
+    </Routes>
+   
+   </BrowserRouter>
 
-    </div>
+
   );
 }
 
